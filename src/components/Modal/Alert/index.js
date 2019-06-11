73b77a9890b8feb702aac9ceb685dcf9d7b208f3
Chris Cedeno <chrisrey95@gmail.com>
2019-06-11 15:28:47 -0400

    Modal date formatting and styling

--- src/components/Modal/Alert/index.js
+++ src/components/Modal/Alert/index.js
@@ -11,15 +11,26 @@ const propTypes = {
 
 
 const Alert = (props) => {
+
+    const formatDate = (date) => {
+        const today = new Date();
+        const newDay = new Date(date)
+        return Math.ceil((today - newDay) / 8.64e7) - 1
+    }
+
     const { title, body, date, image} = props;
+
+    const daysPast = formatDate(date);
+    const dateDisplayString = daysPast > 0 ? `${daysPast} days ago`: 'today'
+
     return (
         <div className="alert-container">
-            <div className="date">{date}</div>
+            <div className="alert-date">{dateDisplayString}</div>
             <div className="alert-info">
                 <h2>{title}</h2>
                 <p>{body}</p>
                 {
-                    image !== "" && <img src={image} alt="preview" />
+                    image !== "" && <img src={image} alt="preview" className="alert-image" />
                 }
             </div>
         </div>
